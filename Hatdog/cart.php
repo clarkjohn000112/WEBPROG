279ec99ade3f24dbc9a31613002c1ed034cc73b8
John Ashley Capistrano <82935030+CapistranoJA@users.noreply.github.com>
2021-11-09 18:32:39 +0800

    Cart.php

--- Hatdog/cart.php
+++ Hatdog/cart.php
@@ -91,9 +91,68 @@
 	    	<div class="item_wrap">
 	    		<div class="item">
 	    			<div class="gallery1">
-  					<a target="_blank" href="C:\Users\63935\Desktop\Hatdog\images\daily.png">
-  					  <img src="C:\Users\63935\Desktop\Hatdog\images\daily.png" alt="Cinque Terre" width="500" height="600">
-  					</a>
+					<?php
+
+include("../Final_SecretShop/partials/connection.php");
+if(empty($_SESSION['cart'])){
+      echo"<h1>There are no items in Cart</h>";
+}
+else{
+// var_dump($_SESSION['cart']);
+echo '<form method="POST" action ="SaveBill.php">
+<table class = "ordertables">
+<tr>
+<th>Item ID</th>
+<th>Item Name</th>
+<th>Quantity</th>
+<th>Price</th>
+<th>Amount</th>
+<th>Action</th>
+</tr>';
+$i=1;
+$tt=explode(",", substr($_SESSION['cart'],1));
+foreach ($tt as $item)
+{
+$qry="select * from foodlist where foodid=$item";
+$run=mysqli_query($con,$qry);
+while ($rows=mysqli_fetch_array($run))
+{
+      if ($i%2==0)
+      {
+            echo "<tr><td>";
+      }
+      else
+      {      
+            echo "<tr><td>";
+      }
+      $i++;      
+      echo $rows["foodid"];
+      echo "</td>";
+      echo "<td>";
+      echo $rows["foodname"];
+      echo "</td>";
+      echo "<td>";
+      echo '<input type="number" name="Quantity[]" value="1" size=5 style="text-align: center;"';
+      echo "</td>";
+      echo "<td>";
+      echo '<input readonly type="number" name="price[]" value="'. $rows["price"].'" size=5 style="text-align: center;" disabled';
+      echo "</td>";
+      echo "<td>";
+      echo $rows["price"];
+      echo "</td>";
+      $ItemID=$rows["foodid"];
+      echo "<td><a href='removeitem.php?key=$ItemID' class = 'modlink'>DELETE</a></td>";
+       
+}
+}      
+}
+?>
+</table>
+<br><br>
+<center>
+<input type="submit" class="icon" Value="Place Order"></button>
+</center>
+</form>
   				</div>
 	    		</div>
 	    		
